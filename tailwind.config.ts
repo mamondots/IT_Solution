df48abd9a89779eb365332f3f09ee5ba29d4dc20
Al Mamon <shikderr112@gmail.com>
2024-11-24 20:16:25 +0600

    start work it solution 11-24-24

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,9 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        rajdhani: ["var(--font-rajdhani)"],
+        rubik: ["var(--font-rubik)"],
       },
     },
   },
